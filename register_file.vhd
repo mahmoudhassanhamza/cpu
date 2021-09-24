ace98c4f435a576274f9360b76b33b8d18973c3f
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-09-24 20:10:45 +0200

    added mem in the senstivity list, now it is working correcrly.

--- register_file.vhd
+++ register_file.vhd
@@ -6,17 +6,19 @@ use ieee.std_logic_signed.all;
 use IEEE.NUMERIC_STD.ALL;
 
 entity register_file is 
+Generic (N : integer := 3;
+M : integer:= 3);
+
 port(
-    -- Generic (N)
-    WD              : in std_logic_vector (3 downto 0) ;
-    WAddr           : in std_logic_vector (3 downto 0) ;
+    WD              : in std_logic_vector (N-1 downto 0) ;
+    WAddr           : in std_logic_vector (M-1 downto 0) ;
     write_sig       : in std_logic;
-    RA              : in std_logic_vector(3 downto 0);
-    RB              : in std_logic_vector(3 downto 0);
+    RA              : in std_logic_vector(M-1 downto 0);
+    RB              : in std_logic_vector(M-1 downto 0);
     RA_sig          : in std_logic;
     RB_sig          : in std_logic;
     rst,clk         : in std_logic;
-    QA,QB           : out std_logic_vector(3 downto 0)
+    QA,QB           : out std_logic_vector(N-1 downto 0)
 );
 end register_file;
 
@@ -25,26 +27,31 @@ architecture behave of register_file is
 -- subtype bitN is bit_vector(3 downto 0); -- N-1
 -- type bitNVector is array(natural range<>) of bitN;
 -- subtype memory_type is bitNVector(3 downto 0 ); --M-1
-type memory_type is array(3 downto 0) of std_logic_vector(3 downto 0 );
+type memory_type is array(M-1 downto 0) of std_logic_vector(N-1 downto 0 );
 signal memr  : memory_type;
 begin 
+    process (RA,RB,RA_sig,RB_sig,memr)
+    begin
 
+        if(RA_sig = '1' ) then   
+             QA <= memr(to_integer(ieee.NUMERIC_STD.UNSIGNED (RA)));
+        end if;
+        if(RB_sig = '1') then
+            QB <= memr(to_integer(ieee.NUMERIC_STD.UNSIGNED (RB)));
+        end if;
+
+    end process;
+------------------------- test writing to a memory cell that you are reading currenlty
     process (clk,rst)
     begin
+
         if (rst = '1') then
-            for i in memr'range loop
-                memr(i) <= "0000";
-            end loop;
-        elsif rising_edge(clk) then
+            memr <= (others => (others => '0') );
+        elsif  rising_edge(clk) then
             if (write_sig = '1') then
                 memr (to_integer(ieee.NUMERIC_STD.UNSIGNED (WAddr))) <= WD;
             end if;
-            if(RA_sig = '1') then
-                QA <= memr(to_integer(ieee.NUMERIC_STD.UNSIGNED (RA)));
-            end if;
-            if(RB_sig = '1') then
-                QB <= memr(to_integer(ieee.NUMERIC_STD.UNSIGNED (RB)));
-            end if;
+
         end if;
     end process;
 
--- register_file_tb.vhd
+++ register_file_tb.vhd
@@ -5,39 +5,43 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_signed.all;
 use IEEE.NUMERIC_STD.ALL;
 
-entity register_file_tb is 
+entity register_file_tb is
+    constant N,M            : integer :=4;
+
 end entity;
 
 architecture behave of register_file_tb is 
 
 component register_file is 
+
+Generic (N,M: integer);
 port(
-    -- Generic (N)
-    WD              : in std_logic_vector (3 downto 0) ;
-    WAddr           : in std_logic_vector (3 downto 0) ;
+    WD              : in std_logic_vector (N-1 downto 0) ;
+    WAddr           : in std_logic_vector (M-1 downto 0) ;
     write_sig       : in std_logic;
-    RA              : in std_logic_vector(3 downto 0);
-    RB              : in std_logic_vector(3 downto 0);
+    RA              : in std_logic_vector(M-1 downto 0);
+    RB              : in std_logic_vector(M-1 downto 0);
     RA_sig          : in std_logic;
     RB_sig          : in std_logic;
     rst,clk         : in std_logic;
-    QA,QB           : out std_logic_vector(3 downto 0)
+    QA,QB           : out std_logic_vector(N-1 downto 0)
 );
 end component;
-signal  WD              :  std_logic_vector (3 downto 0) ;
-signal  WAddr           :  std_logic_vector (3 downto 0) ;
+
+signal  WD              :  std_logic_vector (N-1 downto 0) ;
+signal  WAddr           :  std_logic_vector (M-1 downto 0) ;
 signal  write_sig       :  std_logic;
-signal  RA              :  std_logic_vector(3 downto 0);
-signal  RB              :  std_logic_vector(3 downto 0);
+signal  RA              :  std_logic_vector(M-1 downto 0);
+signal  RB              :  std_logic_vector(M-1 downto 0);
 signal  RA_sig          :  std_logic;
 signal  RB_sig          :  std_logic;
 signal  rst,clk         :  std_logic := '0';
-signal  QA,QB           :  std_logic_vector(3 downto 0);
-
+signal  QA,QB           :  std_logic_vector(N-1 downto 0);
 begin
 
     DUT : register_file
-    port map(WD, WAddr, write_sig, RA,RB, RA_sig, RB_sig, rst, clk, QA, QB);
+   generic map (N,M)
+    port map(WD  , WAddr, write_sig, RA,RB, RA_sig, RB_sig, rst, clk, QA, QB);
     clk <= not clk after 2 ns;
 
 process
@@ -50,16 +54,19 @@ process
         end loop;
         RA_sig <= '1' ;
         RB_sig <= '1' ;
-        for i in 3 downto 0 loop
+        for i in 3 downto 1 loop
             RA     <= conv_std_logic_vector(i,4);
             RB     <= conv_std_logic_vector(i,4);
-            wait for 5 ns;
+            wait for 1 ns;
         end loop;
+        write_sig <= '0';
         rst    <= '1' ;
+        wait for 2 ns;
+        rst <= '0';
         for i in 3 downto 0 loop
             RA     <= conv_std_logic_vector(i,4);
             RB     <= conv_std_logic_vector(i,4);
-            wait for 5 ns;
+            wait for 1 ns;
         end loop;
 end process;
 
