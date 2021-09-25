1a38dae1bcc340ca484cd776e1559fa286dd3cbc
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2021-09-25 21:48:47 +0200

    First implementation of data path, compilation is successful but no test bench done

--- Data_path.vhd
+++ Data_path.vhd
@@ -5,21 +5,23 @@ use ieee.std_logic_signed.all;
 use IEEE.NUMERIC_STD.ALL;
 
 entity Data_path is 
-begin 
-
 Generic (N : integer := 3;
 M : integer:= 3);
 port(
-    WD              : in std_logic_vector (N-1 downto 0) ;
-    WAddr           : in std_logic_vector (M-1 downto 0) ;
-    write_sig       : in std_logic;
-    RA              : in std_logic_vector(M-1 downto 0);
-    RB              : in std_logic_vector(M-1 downto 0);
-    RA_sig          : in std_logic;
-    RB_sig          : in std_logic;
-    rst,clk         : in std_logic;
-    Doutput         : out std_logic_vector(N-1 downto 0);
-    z_f,n_f,o_f     : out std_logic
+    D_WD              : in std_logic_vector (N-1 downto 0) ;
+    D_IE              : in std_logic;
+    D_OE              : in std_logic;
+    ALU_OP            : in std_logic_vector (2 downto 0);
+    ALU_EN            : in std_logic;
+    D_WAddr           : in std_logic_vector (M-1 downto 0) ;
+    D_write_sig       : in std_logic;
+    D_RA              : in std_logic_vector(M-1 downto 0);
+    D_RB              : in std_logic_vector(M-1 downto 0);
+    D_RA_sig          : in std_logic;
+    D_RB_sig          : in std_logic;
+    D_rst,D_clk       : in std_logic;
+    D_output          : out std_logic_vector(N-1 downto 0);
+    D_z_f,D_n_f,D_o_f : out std_logic
 );
 end Data_path;
 
@@ -28,12 +30,12 @@ end Data_path;
 architecture behave of Data_path is 
 
 component alu is 
-    
+Generic (N : integer := 3);
   port
   (
-    a, b        : in  std_logic_vector( 2 downto 0);
-    opcomponent          : in  std_logic_vector( 2 downto 0);
-    y           : out std_logic_vector( 2 downto 0);
+    a, b        : in  std_logic_vector( N-1 downto 0);
+    op          : in  std_logic_vector( N-1 downto 0);
+    y           : out std_logic_vector( N-1 downto 0);
     z_f,n_f,o_f : out std_logic;
     clk,en,reset: in std_logic
   );
@@ -55,11 +57,58 @@ port(
     QA,QB           : out std_logic_vector(N-1 downto 0)
 );
 end component;
+signal MUX_OP      : std_logic_vector(N-1 downto 0);
+signal TEMP_SUM    : std_logic_vector(N-1 downto 0);
+signal temp_QA     : std_logic_vector(N-1 downto 0);    
+signal temp_QB     : std_logic_vector(N-1 downto 0);
+
 begin
 
+MUX: process (D_IE)
+begin
+    if (D_IE = '1') then
+        MUX_OP <= D_WD;
+        else
+        MUX_OP <= TEMP_SUM;
+    end if;
+end process;
+TRI_BUFFER: 
+process(D_OE)
+begin
+    if (D_OE = '1') then
+        D_output <= TEMP_SUM;
+        else
+        D_output <= (others => 'Z');
+    end if;
+end process;
 
+REG_FILE: register_file port map (   
+    WD            => MUX_OP,
+    WAddr         => D_WAddr,
+    write_sig     => D_write_sig,
+    RA            => D_RA,
+    RB            => D_RB,  
+    RA_sig        => D_RA_sig,
+    RB_sig        => D_RB_sig,
+    rst           => D_rst, 
+    clk           => D_clk,
+    QA            => temp_QA,
+    QB            => temp_QB
+    );
 
+ALU_block: alu port map (
+        a        => temp_QA,
+        b        => temp_QB,
+        op       => ALU_OP,
+        y        => TEMP_SUM,
+        z_f      => D_z_f,
+        n_f      => D_n_f,
+        o_f      => D_o_f,
+        clk      => D_clk,
+        en       => ALU_EN,
+        reset    => D_rst      
 
+); 
 
 
 end behave;
---
+++ code.vhd
@@ -0,0 +1,29 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+package micro_assembly_code is
+    constant M : integer := 3;
+    subtype opcode is std_logic_vector(2 downto 0);
+    subtype regs is   std_logic_vector(M-1 downto 0);
+    type instruction is record 
+    IE: std_logic;
+    WrReg,ReadA,ReadB: regs;
+    ALU: opcode;
+    OE: std_logic;
+end record;
+type program is array(natural range<>) of instruction;
+constant OpComp:opcode :="000";
+constant OpAnd:opcode :="001";
+constant OpXor:opcode :="010";
+-- register macros
+constant R0:regs :="000"; -- bit zero is the enable signal
+constant R1:regs :="001";
+constant R2:regs :="010";
+constant R3:regs :="011";
+constant R4:regs :="100";
+constant R5:regs :="101";
+constant R6:regs :="110";
+constant R7:regs :="111";
+end micro_assembly_code;
